dce77990ae3262b3ec7336d0c29ed5fbfe6bd1d5
Jarez25 <jarez179@gmail.com>
2023-04-27 14:01:20 -0600

    primer dias de pyton

---
+++ day1.py
@@ -0,0 +1,44 @@
+frase_bienvenida = 'los 100 dias de python'
+dia = 1
+variable_1 = 'hola'
+variable_2 = variable_1
+valor = 'esta es un valor inicial'
+print(valor)
+valor = 'este es un segudo valor'
+print (valor)
+
+escribe = input('ingresa una frase \n')
+nombre = input('ingresa tu nombre \n') # cometari9o para ingresar tu nombre
+
+# este es un cometarios
+
+
+
+print(frase_bienvenida)
+print(variable_1)
+print(variable_2)
+
+print(dia)
+print(escribe)
+print('hola ' + nombre )
+
+print('estoy en el dia 1 del reto de python')
+
+a = 'rojo'
+b = 'naranja'
+c = 'azul'
+d = 'verde'
+
+
+b = a
+c = b
+print(c)
+
+frace_v1 = 'me llamo sandra'
+frace_v2 = 'y tengo 23 años'
+
+full_frace = (frace_v1 + ' ' + frace_v2)
+
+print(full_frace)
+print(frace_v1 +  frace_v2)
+print(frace_v1 + ' ' +  frace_v2)
---
+++ test_dai1.py
@@ -0,0 +1,9 @@
+print(' hola te voy a realizar unas preguntas')
+
+nombre = input(' cual es tu nombre \n')
+edad = input(' cual es tu edad \n ')
+pais = input(' ingresa tu pais de nacimiento \n')
+
+full = f'tu nombre es {nombre} y tienes {edad} años de edad y naciste en {pais}.'
+
+print(full)
