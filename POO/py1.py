f46ea898912cf2ddc8f0a76888ff2eed33964627
Jarez <jarez@localhost.localdomain>
2023-05-10 06:06:12 -0600

    poo

---
+++ POO/py1.py
@@ -0,0 +1,12 @@
+#clases 
+
+
+class Auto:
+    marca = ''
+    modelo = 0
+    placa = ''
+
+taxi = Auto()
+
+print(taxi)
+print(taxi.modelo)
---
+++ POO/py2.py
@@ -0,0 +1,8 @@
+# classe y objetos II
+
+class persona:
+    doctor = 'carlos'
+    
+persona.doctor
+
+print(persona.doctor)
