f54c9720b199a61221292aa3aea68812f0728435
Jarez25 <jarez179@gmail.com>
2023-05-11 16:50:04 -0600

    decimo-quinto dia en python

---
+++ DIA 15/days15.py
@@ -0,0 +1,35 @@
+# dias de repaso
+
+parrafo = 'este es un texto'
+texto = 'este es un parrafo'
+numero = 1
+numero_dos = 2
+
+print(f'hola {parrafo} y  {texto}')
+
+print(' '.join([parrafo,texto]))
+print(' '.join([parrafo,texto,parrafo,texto])) #
+print('*'.join([parrafo,texto,parrafo,texto]))
+#print('*'.join([parrafo,numero])) aqui ni se pueden mescalr int con str
+#print('*'.join([numero, numero_dos])) al parecer join no admite int
+print('%s %s' % (texto, parrafo))
+print('%s %s' % (texto, numero))
+print('%i %i' % (numero, numero_dos))
+print('%i + %i' % (numero, numero_dos)) #no puedo sumar de esta manera
+
+print('{}'.format(parrafo, texto))
+print('{} {}'.format(parrafo, texto))
+print('{}'.format(parrafo, texto))
+
+mensaje = 'Aquí hay un ejemplo'
+'de como cambiar el ancho'
+'de una imagen al pasar sobre ella'
+
+mensaje2 = ('Aquí hay un ejemplo'
+'de como cambiar el ancho'
+'de una imagen al pasar sobre ella')
+
+
+print(mensaje)
+print(mensaje2)
+print((texto+' ')*3)
---
+++ DIA 15/string.py
@@ -0,0 +1,18 @@
+mensaje = 'este es una cadena'
+buscar = 'este'
+buscar1 = 'hola'
+
+for letras in mensaje:
+    print(letras)
+    
+print(buscar in mensaje)
+
+print(buscar1 in mensaje)
+
+dicc = dict(enumerate(mensaje))
+tupla = tuple(enumerate(mensaje))
+lista = set(enumerate(mensaje))
+
+print(dicc)
+print(tupla)
+print(lista)
